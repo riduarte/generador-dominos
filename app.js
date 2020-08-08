adce8659c4431f706e648911d1dabf21cbcef0c9
riduarte <riduarte88@gmail.com>
2020-08-08 17:31:13 +0000

    a

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var noun = ['jogger','racoon',"cow","beer","geeks"];
 var dom= [".com",".ve",".org"];
 
 for (let i = 0; i < pronoun.length; i++) {
-    for (let i = 0; i < adj.length; i++) {
-        for (let i = 0; i < noun.length; i++) {
-            for (let i = 0; i < dom.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let f = 0; f < noun.length; f++) {
+            for (let r = 0; r < dom.length; r++) {
       
-                console.log(pronoun[i].concat(adj[i]).concat(noun[i]).concat(dom[i]) ) 
+                console.log(pronoun[i].concat(adj[j]).concat(noun[f]).concat(dom[r]) ) 
             }            
         }   
     }
